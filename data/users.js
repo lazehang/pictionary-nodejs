49dfa27a93024cf0d816c91ebb7057fa9f8b49dd
harrixon <harrisonchan1991@gmail.com>
2018-05-08 13:30:06 +0800

    clone working chat-box with auth

---
+++ data/users.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        id : 0,
+        email :"tom@tom.com",
+        password: '$2b$10$GXW2Z3JAdiwgi9SvBQBluuKZV0ycZuY2MPWIbpAOu03y3JphIyI6G'
+    },{
+        id: 1,
+        email : "peter@peter.com",
+        password: '$2b$10$.or2FgGdXToN0X1FKzO4V.yEg0HxIZ555ZkqbxqbZM/HjgQjUSOFm'
+    },{
+        id: 2,
+        email : "tom2@tom.com",
+        password: '$2b$10$GXW2Z3JAdiwgi9SvBQBluuKZV0ycZuY2MPWIbpAOu03y3JphIyI6G'
+    }
+];
